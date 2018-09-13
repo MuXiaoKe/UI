4f64ca97a5eb32318c3b343bad93c5e33fa6df67
MuXiaoKe <745633560@qq.com>
2018-09-13 20:20:04 +0800

    更新dialog

--- dialog/dialog.js
+++ dialog/dialog.js
@@ -1,5 +1,55 @@
 //zh diaolog
 (function(win) {
 	//默认配置
-	var config = {};
+	var config = {
+		type: 0,
+		shade: true,
+		shadeClose: true,
+		fixed: true,
+		animate: "scale"
+	};
+	var dCommon = {
+		extend: function() {
+			var configcopy = JSON.parse(JSON.stringify(config));
+			for (var i in obj) {
+				configcopy[i] = obj[i];
+			}
+			return configcopy;
+		},
+		timer: {}, //定时器
+		end: {} //结束
+	};
+	var index = 0,
+		defaultclass = ["mdialog-layer"],
+		Dialog = function(options) {
+			this.config = dCommon.extend(options);
+			this.htm();
+		};
+	Dialog.prototype.htm = function() {
+		config = this.config;
+		var dialogWrap = document.createElement("div");
+		this.id = dialogWrap.id = defaultclass[0] + index;
+
+		dialogWrap.setAttribute(
+			"class",
+			defaultclass[0] + " " + defaultclass[0] + (dCommon.type || 0)
+		);
+		dialogWrap.setAttribute("index", index);
+		var title = "<h3>" + config.title + "</h3>";
+
+		var button = (function() {
+			//如果是单个按钮字符串，改为数组
+			typeof config.btn === "string" && (config.btn = [config.btn]);
+			var btnlength = (config.btn || []).length,
+				btnhtml;
+			//如果没有btn，返回空
+			if (btnlength === 0 || !config.btn) return "";
+
+			config.btn.forEach(function(item, index) {
+				btnhtml = btnhtml + "<span >" + config.btn[index] + "</span>";
+			});
+
+			return '<div class="dialog-layerbtn">' + btndom + "</div>";
+		})();
+	};
 })(window);
