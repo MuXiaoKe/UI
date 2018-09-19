c9c977eb62a8ab26f3efa59574b04e2bf2bae051
MuXiaoKe <745633560@qq.com>
2018-09-19 20:15:28 +0800

    update dialog

--- dialog/dialog.js
+++ dialog/dialog.js
@@ -25,7 +25,7 @@
 			this.config = dCommon.extend(options);
 			this.htm();
 		};
-	Dialog.prototype.htm = function() {
+	Dialog.prototype.render = function() {
 		config = this.config;
 		var dialogWrap = document.createElement("div");
 		this.id = dialogWrap.id = defaultclass[0] + index;
@@ -57,6 +57,74 @@
 			config.style = config.style || "";
 			config.style +=
 				" top:" + (document.body.scrollTop + config.top) + "px";
+		}
+
+		if (config.type === 2) {
+			config.content =
+				'<i></i><i class="layui-m-layerload"></i><i></i><p>' +
+				(config.content || "") +
+				"</p>";
+		}
+		if (config.skin) config.anim = "up";
+		if (config.skin === "msg") config.shade = false;
+
+		dialogWrap.innerHTML =
+			(config.shade
+				? "<div " +
+				  (typeof config.shade === "string"
+						? 'style="' + config.shade + '"'
+						: "") +
+				  ' class="mdialog-layershade"></div>'
+				: "") +
+			'<div class="mdialog-layermain" ' +
+			(!config.fixed ? 'style="position:static;"' : "") +
+			">" +
+			'<div class="mdialog-layersection">' +
+			'<div class="mdialog-layerchild ' +
+			(config.skin ? "mdialog-layer-" + config.skin + " " : "") +
+			(config.className ? config.className : "") +
+			" " +
+			(config.anim ? "mdialog-anim-" + config.anim : "") +
+			'" ' +
+			(config.style ? 'style="' + config.style + '"' : "") +
+			">" +
+			title +
+			'<div class="mdialog-layercont">' +
+			config.content +
+			"</div>" +
+			button +
+			"</div>" +
+			"</div>" +
+			"</div>";
+
+		if (!config.type || config.type === 2) {
+			var dialogs = document.getElementsByClassName(
+				defaultclass[0] + config.type
+			);
+			var dialoglength = dialogs.length;
+
+			if (dialoglength >= 1) {
+				dialog.close(dialogs[0].getAttribute("index"));
+			}
+		}
+
+		document.body.appendChild(dialogWrap);
+		var el = document.querySelectorAll("#" + this.id)[0];
+		config.success && config.success(el);
+
+		this.index = index++;
+		this.action(config, el);
+	};
+	Dialog.prototype.action = function(config, el) {
+		var that = this;
+		// 延迟自动关闭
+		if (config.time) {
+			dCommon.timer[that.index] = setTimeout(function() {
+				dialog.close(that.index);
+			}, config.time * 1000);
+		}
+
+		if (config.btn) {
 		}
 	};
 })(window);
